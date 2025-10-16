ffea3c0dee15e8c5bea4c751280e91bd3da94caf
BurgerMan90001 <150270423+BurgerMan90001@users.noreply.github.com>
2025-10-16 08:31:31 -0500

    head

---
+++ src/goop.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "../header/goop.h"
+
+void test() {
+    std::cout << "Ohai from another .cpp file!";
+    std::cin.get();
+
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,9 @@
 
 #include <iostream>
+#include "../header/goop.h"
 
 int main() {
     std::cout << "Hello, Worldsdajkasukdhaushdkahskd!" << std::endl;
+    test();
     return 0;
 }
